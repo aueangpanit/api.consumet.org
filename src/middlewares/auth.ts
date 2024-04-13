c85f9cd90a51f53c9520d4ee4d9c1a10d5a07af9
Aue Angpanitcharoen <aueangpanit@gmail.com>
2024-04-13 22:18:12 +0100

    Use updated api key name to verify auth status

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 export default async function auth(request: any, reply: any) {
   const apiKey = request.headers['x-api-key'];
-  const knownKey = process.env.APIKEY;
+  const knownKey = process.env.API_KEY;
 
   if (!apiKey || apiKey !== knownKey) {
     return reply.code(401).send({ error: 'Unauthorized' });
